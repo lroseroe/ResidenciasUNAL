654e3505459d4d89b668cbb2abb522f555c618a5
lroseroe <lroseroe@unal.edu.co>
2025-06-25 19:48:47 -0500

    MinHeap implementado

---
+++ src/main/java/estructuras/MinHeap.java
@@ -0,0 +1,101 @@
+package estructuras;
+
+public class MinHeap<T extends Comparable<T>>{
+    T[] array;
+    int maxSize;
+    int size;
+    
+    public MinHeap(int n){
+        maxSize = n;
+        array = (T[]) new Comparable[maxSize];
+        size = 0;
+    }
+    
+    public int parent(int i){
+        return (i-1)/2;
+    }
+    
+    public int leftChild(int i){
+        return 2*i + 1;
+    }
+    
+    public int rightChild(int i){
+        return 2*i + 2;
+    }
+    
+    public void swap(int i, int j){
+        T temp = array[j];
+        array[j] = array[i];
+        array[i] = temp;
+        
+    }
+    
+    public void siftUp(int i){
+        while(i > 0 && array[parent(i)].compareTo(array[i]) > 0){ //Si el padre es mayor, hay que hacer swap
+            swap(i, parent(i));
+            i = parent(i);
+        }
+    }
+    
+    public void siftDown(int i){
+        int maxIndex = i;
+        
+        int left = leftChild(i);
+        if(left < size && array[left].compareTo(array[maxIndex]) < 0){
+            maxIndex = left;
+        }
+        
+        int right = rightChild(i);
+        if(right < size && array[right].compareTo(array[maxIndex]) < 0){
+            maxIndex = right;
+        }
+        
+        if(i != maxIndex){
+            swap(i, maxIndex);
+            siftDown(maxIndex);
+        }
+    }
+    
+    public void insert(T item){
+        if(size == maxSize){
+            throw new RuntimeException("Heap is full.");
+        }
+        array[size] = item;
+        siftUp(size);
+        size ++;
+    }
+    
+    public T extractMin(){
+        T result = array[0];
+        array[0] = array[--size];
+        siftDown(0);
+        return result;
+    }
+    
+    public void remove(int i){
+        if(i < 0 || i > size - 1){
+            throw new RuntimeException("Index not in Heap.");
+        }
+        array[i] = array[0]; //Ponerlo de primero obligatoriamente
+        siftUp(i);
+        extractMin();
+    }
+    
+    public void changePriority(int i, T item){
+        T oldItem = array[i];
+        array[i] = item;
+        if(item.compareTo(oldItem) > 0){
+            siftUp(i);
+        } else {
+            siftDown(i);
+        }
+    }
+    
+    //Para testeos
+    public void print(){
+        for(int i = 0; i < size; i++){
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+}
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,15 @@
 package main;
 
+import estructuras.MinHeap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Testeando...");
+        MinHeap<Integer> heap = new MinHeap<>(10);
+        heap.insert(7);
+        heap.insert(10);
+        heap.insert(4);
+        
+        heap.print();
     }
 }
+
